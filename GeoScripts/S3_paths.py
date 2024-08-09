4339c1a6d8a2c47b7392f3fa6b95b9e00a96e6df
Oscar BAUTISTA <oscar.bautista@wfp.org>
2024-08-09 11:24:54 +0200

    updated and fixed S3_paths function

--- GeoScripts/S3_paths.py
+++ GeoScripts/S3_paths.py
@@ -1,5 +1,5 @@
 def replace_local_to_s3(data):
-  """
+    """
   Replaces local file paths with S3 paths in a dictionary.
 
   Args:
@@ -8,14 +8,19 @@ def replace_local_to_s3(data):
   Returns:
       A new dictionary with S3 paths replacing the local paths.
   """
-  new_data = {}
-  for key, value in data.items():
-    new_value = {}
-    for inner_key, inner_value in value.items():
-      if inner_key == "mask_shp":
-        # Replace local path with S3 path
-        new_value[inner_key] = f"s3://geotar.s3.hq/{inner_value.split('/')[-2:]}"
-      else:
-        new_value[inner_key] = inner_value
-    new_data[key] = new_value
-  return new_data
+    new_data = {}
+    for key, value in data.items():
+        new_value = {}
+        for inner_key, inner_value in value.items():
+            if inner_key == "mask_shp":
+                # Replace local path with S3 path
+                base_path = "C:/"
+                if inner_value.startswith(base_path):
+                    relative_path = inner_value[len(base_path):].replace("\\", "/")
+                    new_value[inner_key] = f"s3://geotar.s3.hq/{relative_path}"
+                else:
+                    new_value[inner_key] = inner_value
+            else:
+                new_value[inner_key] = inner_value
+        new_data[key] = new_value
+    return new_data
